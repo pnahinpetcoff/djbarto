eeb3fa90d6ad3dd6dcd810e7ef13db7464bd525b
Pablo Nahin Petcoff <pablo.nahinpetcoff@icbc.com.ar>
2023-04-14 18:02:19 -0300

    cambio sw

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,7 +3,7 @@
 import { register } from 'register-service-worker'
 
 console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
