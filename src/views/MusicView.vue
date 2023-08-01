f72e9758fd0b3cbec0df797943731a6e61fe4cbb
Pablo Nahin Petcoff <pablo.nahinpetcoff@icbc.com.ar>
2023-07-31 23:56:03 -0300

    agrego video posadas experience

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -10,6 +10,10 @@
           <h2 class="title">{{'MUSIC'}}</h2>
         </v-col>
         <v-col cols="12">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/BURSwutSz9o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+          </iframe>
+        </v-col>
+        <v-col cols="12">
           <iframe title="SoundCloud" width="100%" height="200" scrolling="no" frameborder="yes" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1597418329&color=000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
           </iframe>
           <div style="font-size: 10px; color: #000000;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
